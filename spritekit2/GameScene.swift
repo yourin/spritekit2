b0d27bc860a99f54233f05af607509172fcd40ba
youringtone <youringtone@gmail.com>
2015-04-17 07:01:04 +0900

    test
    test

--- spritekit2/GameScene.swift
+++ spritekit2/GameScene.swift
@@ -27,8 +27,8 @@ class GameScene: SKScene {
             
             let sprite = SKSpriteNode(imageNamed:"Spaceship")
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
+            sprite.xScale = 0.2
+            sprite.yScale = 0.2
             sprite.position = location
             
             let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
